2155e6cae0dcc3aa15c1916f65f6a1695e6b0e54
Vineet Mundhra <vineet.mundhra@gmail.com>
2011-10-22 22:09:10 -0700

    more changes to getpid

--- getpid.c
+++ getpid.c
@@ -11,17 +11,22 @@
 #define DEF_ITERS 100
 #define DEF_RNDS 1
 #define DEF_TEST 1
+#define DEF_FREQ 800 // * 10^6 cycles per second
+#define DEF_OUTPUT "out.data"
+
+int verbose = 0;
 
 void print_help ()
 {
-	printf("Usage -n <num_iterations> -r <num_rounds> -t <test_num>\n");	
+	printf("Usage -n <num_iterations> -r <num_rounds> -t <test_num> -f <cpu freq in MHz>\n");	
 }
 
-
-void test_get_pid_today(int num_rounds, int num_iters) {
+void test_get_pid_today(int num_rounds, int num_iters, FILE *fp) 
+{
 	int rnd, i;
 	struct timeval st_time, end_time;
 	unsigned long mytime;	
+	unsigned long avg_time = 0;	
 
 	for (rnd = 0; rnd < num_rounds; rnd++) {
 		gettimeofday(&st_time, NULL);
@@ -32,13 +37,25 @@ void test_get_pid_today(int num_rounds, int num_iters) {
 
 		mytime = ((end_time.tv_sec - st_time.tv_sec) * 1000000) +  end_time.tv_usec - st_time.tv_usec;
 
-		printf("Iter %d time %lu mus\n", rnd, mytime);
+		mytime = (mytime * 1000) / num_iters;
+
+		avg_time = avg_time + mytime;
+		if (verbose) {
+			printf("Iter %d time %lu ns\n", rnd, mytime);
+		}
+		fprintf(fp, "%lu\n", mytime); 
 	}
+
+	printf("Average %lu", avg_time / num_rounds);
+
 }
 
-void test_get_pid_rdtsc(int num_rounds, int num_iters) {
+void test_get_pid_rdtsc(int num_rounds, int num_iters, int cpu_freq, FILE *fp) 
+{
 	int rnd, i;
-	unsigned long long start, end;	
+	unsigned long long start, end;
+	unsigned long long mytime;
+	unsigned long long total_time = 0;
 
 	for (rnd = 0; rnd < num_rounds; rnd++) {
 		start = rdtsc();
@@ -47,42 +64,97 @@ void test_get_pid_rdtsc(int num_rounds, int num_iters) {
 		}
 		end = rdtsc();
 
-		printf("Iter %d time %llu cycles\n", rnd, end - start);
-	}
-}
+		mytime = ((end - start ) * 1000) / (cpu_freq * num_iters);
+		total_time = total_time + mytime;
 
-void test_devnull_rdtsc(int num_rounds, int num_iters) {
-	int rnd, i;
-	unsigned long long start, end;	
-
-	for (rnd = 0; rnd < num_rounds; rnd++) {
-		start = rdtsc();
-		for (i = 0; i < num_iters; i++) {
-			system("echo 'hello' > /dev/null");
+		if (verbose) {
+			printf("Iter %d time %llu ns\n", rnd, mytime);
 		}
-		end = rdtsc();
-
-		printf("Iter %d time %llu cycles\n", rnd, end - start);
+		fprintf(fp, "%llu\n", mytime);
 	}
+
+	printf("Avg time :%llu\n", total_time / num_rounds);
 }
 
-void test_devnull_today(int num_rounds, int num_iters) {
+void test_devnull_today(int num_rounds, int num_iters, FILE *fp) 
+{
 	int rnd, i;
 	struct timeval st_time, end_time;
 	unsigned long mytime;	
+	unsigned long avg_time = 0;	
+
+	int fid;
+	char *buf = "buffer";
+	int size = sizeof(buf);
+
+	fid = open("/dev/null", O_RDWR);
+	if (fid < 0) {
+		perror("/dev/null open failed\n");
+		return;
+	}
 
 	for (rnd = 0; rnd < num_rounds; rnd++) {
 		gettimeofday(&st_time, NULL);
 		for (i = 0; i < num_iters; i++) {
-			system("echo 'hello' > /dev/null");
+			write(fid, buf, size);	
 		}
 		gettimeofday(&end_time, NULL);
 
 		mytime = ((end_time.tv_sec - st_time.tv_sec) * 1000000) +  end_time.tv_usec - st_time.tv_usec;
 
-		printf("Iter %d time %lu mus\n", rnd, mytime);
+		mytime = (mytime * 1000) / num_iters;
+
+		avg_time = avg_time + mytime;
+		if (verbose) {
+			printf("%d time %lu ns\n", rnd, mytime);
+		}
+		fprintf(fp, "%lu\n", mytime); 
 	}
-}		
+
+	printf("gettimeofday - /dev/null write Average %lu", avg_time / num_rounds);
+	close(fid);
+}
+
+void test_devnull_rdtsc(int num_rounds, int num_iters, int cpu_freq, FILE *fp) 
+{
+	int rnd, i;
+	unsigned long long start, end;
+	unsigned long long mytime;
+	unsigned long long total_time = 0;
+
+	int fid;
+	char *buf = "buffer";
+	//char rd_buf[10];
+	int size = sizeof(buf);
+
+	fid = open("/dev/null", O_RDWR);
+	if (fid < 0) {
+		perror("/dev/null open failed\n");
+		return;
+	}
+
+	for (rnd = 0; rnd < num_rounds; rnd++) {
+		start = rdtsc();
+		for (i = 0; i < num_iters; i++) {
+			write(fid, buf, size);	
+			//read(fid, rd_buf, size);	
+		}
+		end = rdtsc();
+
+		mytime = ((end - start ) * 1000) / (cpu_freq * num_iters);
+		total_time = total_time + mytime;
+
+
+		if (verbose) {
+			printf("Iter %d time %llu ns\n", rnd, mytime);
+		}
+
+		fprintf(fp, "%llu\n", mytime);
+	}
+
+	printf("Avg time :%llu\n", total_time / num_rounds);
+	close(fid);
+}
 
 int main(int argc, char **argv)
 {
@@ -90,10 +162,12 @@ int main(int argc, char **argv)
 	int num_rounds = DEF_RNDS;
 	int test_num = DEF_TEST;
 	char c;
-
+	char *data_file = DEF_OUTPUT;
+	int cpu_freq = DEF_FREQ;
+	FILE *fid;
 	// Get opt setup	
 
-	while ((c = getopt(argc, argv, "r:n:t:h")) != -1) {
+	while ((c = getopt(argc, argv, "r:n:t:f:o:v:h")) != -1) {
 		switch(c) {
 
 			case 'r':
@@ -108,6 +182,19 @@ int main(int argc, char **argv)
 			test_num = atoi(optarg);
 			break;
 			
+			case 'f':
+			cpu_freq = atoi(optarg);
+			break;
+
+			case 'o':
+			data_file = optarg;
+			break;
+			
+
+			case 'v':
+			verbose = 1;
+			break;
+
 			case 'h':
 			print_help();
 			break;
@@ -119,28 +206,31 @@ int main(int argc, char **argv)
 	
 		}
 
+		fid = fopen(data_file, "w");
+
 		switch(test_num) {
 		
 		case 1:
-		test_get_pid_today(num_rounds, num_iters);
+		test_get_pid_today(num_rounds, num_iters, fid);
 		break;
 
 		case 2:
-		test_devnull_today(num_rounds, num_iters);
+		test_devnull_today(num_rounds, num_iters, fid);
 		break;
 
 		case 11:
-		test_get_pid_rdtsc(num_rounds, num_iters);
+		test_get_pid_rdtsc(num_rounds, num_iters, cpu_freq, fid);
 		break;
 		
 		case 12:
-		test_devnull_rdtsc(num_rounds, num_iters);
+		test_devnull_rdtsc(num_rounds, num_iters, cpu_freq, fid);
 		break;
 
 		default:
 		break;
 		}
 
+		fclose(fid);
 	return 0;
 }
 
