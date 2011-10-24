9bc74cb487093dd8076befbe45ffa8af641050cc
Vineet Mundhra <vineet.mundhra@gmail.com>
2011-10-24 02:15:21 -0700

    more cleanup

--- run_syscall.sh
+++ run_syscall.sh
@@ -2,5 +2,5 @@
 
 for i in {1..10000}
 do
-  ./syscall_time -r 1 -n 1 -a 
+  ./all_tests -r 1 -n 1 -a 
 done
--- run_tests.sh
+++ run_tests.sh
@@ -1,10 +1,10 @@
 #/bin/sh
 
 # Measure cached system call time
-./syscall_time -t 1 -o cached.data -a -w  
+./all_tests -t 1 -o cached.data -a -w  
 
 # Measure uncached system call time
 ./run_syscall.sh
 
 # Measure process creation time 
-./syscall_time -t 2 -o creat.data 
+./all_tests -t 2 -o creat.data 
