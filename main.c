1095508b9d82e3fcc7d8bda1f3466922e142e413
Phuc Xuan Nguyen <phuc@pxnguyen.com>
2011-10-24 18:16:17 -0700

    More change to main.c

--- main.c
+++ main.c
@@ -5,45 +5,45 @@ void measurement_overhead(int N){
 	unsigned long long a,b,c;
 	
 	int i;
+	int index = 0;
 	
 	a = rdtsc();
 	for(i = 0; i < N; i++){
-		i = i;
+		index++;
 	}
 	c = rdtsc();
 	printf("Without rdtsc: %f\n", (c-a)/(double)N);
 	
 	a = rdtsc();
 	for(i = 0; i < N; i++){
-		i = i;
+		index++;
 		rdtsc();
 	}
 	c = rdtsc();
-	printf("Without rdtsc: %f\n", (c-a)/(double)N);
+	printf("With rdtsc: %f\n", (c-a)/(double)N);
+}
+
+void measure_random(int N){
+	unsigned long long a,b,c;
+	int i;
+        int index = 0;
+
+        a = rdtsc();
+        index++;
+        c = rdtsc();
+        printf("Measuring random: %f\n", (c-a)/(double)N);
 }
 
 void measure_forloop_overhead(int N){
 	unsigned long long a,b,c;
-	a = rdtsc();
-	rdtsc();
-	rdtsc();
-	rdtsc();
-	rdtsc();
-	rdtsc();
-	rdtsc();
-	rdtsc();
-	rdtsc();
-	rdtsc();
-	c = rdtsc();
-	printf("Without for loop: %f\n", (c-a)/10.0);
-	
-	a = rdtsc();
 	int i;
-	for(i = 0; i < 10; i++){
-		rdtsc();
+	int index = 0;
+	a = rdtsc();
+	for(i = 0; i < N; i++){
+		index++;
 	}
 	c = rdtsc();
-	printf("With for loop: %f\n", (c-a)/10.0);
+	printf("With for loop: %f\n", (c-a)/(double)N);
 }
 
 void foo(){
@@ -71,9 +71,35 @@ void foo4(int a1,int a2, int a3, int a4){
 	int y = x;
 }
 
+void foo5(int a1,int a2, int a3, int a4, int a5){
+        int x = 1+1;
+        int y = x;
+}
+
+void foo6(int a1,int a2, int a3, int a4, int a5, int a6){
+        int x = 1+1;
+        int y = x;
+}
+
+void foo7(int a1,int a2, int a3, int a4, int a5, int a6, int a7){
+        int x = 1+1;
+        int y = x;
+}
+
+
+
+
 void measure_procedure_call(int N){
 	unsigned long long a,b,c;
 	int i;
+
+	a = rdtsc();
+        for(i = 0; i < N; i++){
+                int x = 1+1;
+		int y = x;
+        }
+	c = rdtsc();
+	printf("No procedure call: %f\n", (c-a)/(double)N);
 	
 	a = rdtsc();
 	for(i = 0; i < N; i++){
@@ -102,11 +128,41 @@ void measure_procedure_call(int N){
 	}
 	c = rdtsc();
 	printf("foo3(): %f\n", (c-a)/(double)N);
+
+	a = rdtsc();
+        for(i = 0; i < N; i++){
+                foo4(i,i,i,i);
+        }
+        c = rdtsc();
+        printf("foo4(): %f\n", (c-a)/(double)N);
+
+	a = rdtsc();
+        for(i = 0; i < N; i++){
+                foo5(i,i,i,i,i);
+        }
+        c = rdtsc();
+        printf("foo5(): %f\n", (c-a)/(double)N);
+
+
+	a = rdtsc();
+        for(i = 0; i < N; i++){
+                foo6(i,i,i,i,i,i);
+        }
+        c = rdtsc();
+        printf("foo6(): %f\n", (c-a)/(double)N);
+
+	a = rdtsc();
+        for(i = 0; i < N; i++){
+                foo7(i,i,i,i,i,i,i);
+        }
+        c = rdtsc();
+        printf("foo7(): %f\n", (c-a)/(double)N);
 }
 
 int main(int argc, char* argv[]){
 	measurement_overhead(100000);
 	measure_forloop_overhead(100000);
-	measure_procedure_call(100000);
+	measure_procedure_call(100000000);
+	measure_random(10);
 	return 0;
 }
